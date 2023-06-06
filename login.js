4b0f24c1f9e8a71476577d6b3acaa04156f5a32b
x111444 <50602754+x111444@users.noreply.github.com>
2023-06-06 14:45:26 +0900

    11

--- login.js
+++ login.js
@@ -138,14 +138,14 @@ app.post('/api/signup', (req, res) => {
     console.log(req.body);
     const { user_id, user_pw, user_lv, user_mail, user_name, phone_number } = req.body;
     // MySQL 데이터베이스에서 사용자 정보를 확인합니다.
-    connection.query('SELECT * FROM user_data WHERE user_id = ?', [user_id], (error, results) => {
+    connection.query('SELECT * FROM users WHERE user_id = ?', [user_id], (error, results) => {
         if (error) {
             console.error('MySQL 쿼리 오류:', error);
             res.status(500).json({ success: false, message: 'Internal server error' });
         }
         else if (Array.isArray(results) && results.length != 0) {
             // 사용자 정보가 없으면 로그인 실패 처리를 합니다.
-            res.status(401).json({ success: false, message: 'username is Exit' });
+            res.status(401).json({ success: false, message: 'user_id is Exit' });
         }
         else {
             // 로그인 성공 처리를 합니다.

7dde00873e37889e0893642b56c5dfb1291d459b
x111444 <50602754+x111444@users.noreply.github.com>
2023-06-09 11:03:18 +0900

    11

--- login.js
+++ login.js
@@ -145,6 +145,7 @@ app.post('/api/login', (req, res) => {
 app.get('/api/checkLogin', (req, res) => {
     const  {id}  = req.query;
     console.log(req.query)
+    console.log(req)
     console.log(req.cookies , id)
     // 세션에 저장된 사용자 정보가 있는지 및 사용자 ID와 일치하는지 확인
     if (req.cookies.id === id) {

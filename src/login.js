1a2abd1fdfaece371e585d07df793ae683b18007
x111444 <50602754+x111444@users.noreply.github.com>
2023-10-04 22:23:09 +0900

    11

--- src/login.js
+++ src/login.js
@@ -523,6 +523,7 @@ app.get('/api/diary/animal', async (req, res) => {
                 }
             }
             //console.log(animal_results)
+            console.log("?")
             return res.status(200).send(animal_results)
             
                

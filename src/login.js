c5d11acf3398046e3d7590b7e4e915cb0586506e
x111444 <50602754+x111444@users.noreply.github.com>
2023-10-07 19:45:36 +0900

    1

--- src/login.js
+++ src/login.js
@@ -124,6 +124,7 @@ app.post('/api/login', (req, res) => {
             else {
                 // 로그인 성공 처리를 합니다.
                 console.log(results)
+                console.log(results[0].user_level)
                 req.session.username = username
                 req.session.userlevel = results[0].user_level
                 res.json({ success: true, message: 'Login successful', user: username });
